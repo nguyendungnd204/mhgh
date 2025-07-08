02acd48c6e45ccf0aca67e9c9dbec6da8df78b03
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-07-08 21:22:41 +0700

    update news migrate

--- database/migrations/2025_07_08_140243_create_events_table.php
+++ database/migrations/2025_07_08_140243_create_events_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumbnail')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
