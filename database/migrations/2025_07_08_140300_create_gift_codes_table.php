382b1e0771a904f48034322efa4a4522e18cfc28
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-07-08 21:28:32 +0700

    update gift_codes migrate

--- database/migrations/2025_07_08_140300_create_gift_codes_table.php
+++ database/migrations/2025_07_08_140300_create_gift_codes_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('gift_codes', function (Blueprint $table) {
             $table->id();
+            $table->string('code')->unique();
+            $table->string('reward');
+            $table->timestamp('expired_at')->nullable();
+            $table->integer('max_uses')->nullable();
+            $table->integer('used_count')->default(0);
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
