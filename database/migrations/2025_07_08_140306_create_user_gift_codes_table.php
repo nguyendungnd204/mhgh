557bd174b0d69766b85a57eace210090b8895789
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-07-08 21:29:17 +0700

    update user_gift_codes migrate

--- database/migrations/2025_07_08_140306_create_user_gift_codes_table.php
+++ database/migrations/2025_07_08_140306_create_user_gift_codes_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_gift_codes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('gift_code_id')->constrained('gift_codes')->onDelete('cascade');
             $table->timestamps();
         });
     }
